ce2759cd8b6ccff0fe31ab94b5b69fc6fe274bce
Yurii Yakubin <yurii.yakubin@gmail.com>
2025-02-17 22:07:46 +0000

    Removed VariablesModule

--- scripts/inject/InjectContext.mjs
+++ scripts/inject/InjectContext.mjs
@@ -2,8 +2,6 @@ import path from 'node:path';
 import url from 'node:url';
 
 import { saveIfDifferent } from "###/utils/FileSystem.js";
-import * as CXX from "###/utils/CXX.js";
-import * as CMake from "###/utils/CMake.js";
 
 async function linesSaveTo(filename, lines)
 {
@@ -32,184 +30,11 @@ class InjectHooks {
   }
 };
 
-function transformCMakeConfigToObject(config)
-{
-  const result = {};
-  for (const [key, entry] of Object.entries(config)) {
-    if (entry.type === "boolean") {
-      result[key] = entry.value.toUpperCase() === "ON" || entry.value.toUpperCase() === "TRUE";
-    }
-    else if (entry.type === "number") {
-      result[key] = entry.value;
-    }
-    else if (entry.type === "string") {
-      result[key] = entry.value;
-    }
-    else if (entry.type === "strings") {
-      result[key] = entry.value ? entry.value.split(";") : [];
-    }
-    else if (Array.isArray(entry.type)) {
-      result[key] = entry.value;
-    }
-    else {
-      throw `Not supported type ${entry.type}`;
-    }
-  }
-  return result;
-}
-
-class VariablesModule {
-  _ctx;
-  _initConfig = {};
-
-  constructor(ctx)
-  {
-    this._ctx = ctx;
-  }
-
-  async loadConfig(filename)
-  {
-    const fileUrl = url.pathToFileURL(filename);
-    const module = await import(fileUrl);
-    this._initConfig = module.default;
-  }
-
-  async triggerEvent()
-  {
-    await this._ctx.hooks.emit("variables.init", this._initConfig);
-  }
-
-  async saveInitConfigAsCMake(filename, print)
-  {
-    let maxLength = 0;
-    for (const name of Object.keys(this._initConfig))
-      maxLength = Math.max(maxLength, name.length);
-    const leader = " .".repeat((maxLength + 4) / 2);
-  
-    const lines = [];
-  
-    lines.push(CMake.generatedScriptNameComment(this._ctx.entryScript));
-    for (const [name, entry] of Object.entries(this._initConfig)) {
-      const space = entry.depends ? "  " : "";
-      if (entry.depends) {
-        lines.push(`if (${entry.depends})`);
-      }
-      const description = entry.description || "";
-      if (typeof entry.value === "boolean") {
-        lines.push(`${space}option(${name} "${description}" ${entry.value ? "ON" : "OFF"})`);
-      }
-      else if (typeof entry.value === "number") {
-        lines.push(`${space}set(${name} ${entry.value} CACHE STRING "${description}")`);
-      }
-      else if (typeof entry.value === "string") {
-        lines.push(`${space}set(${name} "${entry.value}" CACHE STRING "${description}")`);
-        if (Array.isArray(entry.type)) {
-          const strings = entry.type.map((i) => '"' + i + '"').join(' ');
-          lines.push(`${space}set_property(CACHE ${name} PROPERTY STRINGS ${strings})`);
-        }
-      }
-      else if (Array.isArray(entry.value)) {
-        lines.push(`${space}set(${name} "${entry.value}" CACHE STRING "${description}")`);
-      }
-      else {
-        throw `Not supported value of ${entry.value}`;
-      }
-      if (print) {
-        lines.push(`${space}message(STATUS "${name}:${leader.substring(name.length)} \${${name}}")`);
-      }
-      if (entry.depends) {
-        lines.push("endif ()");
-      }
-      lines.push("");
-    }
-  
-    await linesSaveTo(filename, lines);
-  }
-  
-  async saveInitConfigAsCHeader(filename)
-  {
-    const lines = [];
-  
-    lines.push(CXX.generatedScriptNameComment(this._ctx.entryScript));
-    lines.push("");
-  
-    for (const [name, entry] of Object.entries(this._initConfig)) {
-      if (entry.description) {
-        lines.push(`/* ${entry.description} */`);
-      }
-      if (typeof entry.value === "boolean") {
-        lines.push(`#cmakedefine01 ${name}`);
-      }
-      else if (typeof entry.value === "number") {
-        lines.push(`#define ${name} @${name}@`);
-      }
-      else if (typeof entry.value === "string") {
-        lines.push(`#define ${name} "@${name}@"`);
-      }
-      else if (Array.isArray(entry.value)) {
-        lines.push(`#define ${name} "@${name}@"`);
-      }
-      else {
-        throw `Not supported value of ${entry.value}`;
-      }
-      lines.push("");
-    }
-  
-    await linesSaveTo(filename, lines);
-  }
-  
-  async saveInitConfigAsModule(filename)
-  {
-    const lines = [];
-
-    lines.push(CXX.generatedScriptNameComment(this._ctx.entryScript));
-    lines.push("export default {");
-
-    for (const [name, entry] of Object.entries(this._initConfig)) {
-      let type = entry.type;
-      let value = `@${name}@`;
-
-      if (type === undefined) {
-        if (typeof entry.value === "boolean") {
-          type = "boolean";
-        }
-        else if (typeof entry.value === "number") {
-          type = "number";
-        }
-        else if (typeof entry.value === "string") {
-          type = "string";
-        }
-        else if (Array.isArray(entry.value)) {
-          type = "strings";
-        }
-        else {
-          throw `Not supported value of ${entry.value}`;
-        }
-      }
-
-      if (type !== "number") {
-        value = '"' + value + '"';
-      }
-
-      lines.push(`  ${name}: {`);
-      lines.push(`    type: ${JSON.stringify(type)},`);
-      lines.push(`    value: ${value},`);
-      lines.push(`  },`);
-    }
-
-    lines.push("};");
-    lines.push("");
-
-    await linesSaveTo(filename, lines);
-  }
-};
-
 export class InjectContext {
   _args;
   _entryScript;
   _config = {};
   _plugins = [];
-  _variables;
 
   _hooks = new InjectHooks;
 
@@ -226,7 +51,6 @@ export class InjectContext {
   {
     this._args = args;
     this._entryScript = args.script;
-    this._variables = new VariablesModule(this);
   }
 
   get args()
@@ -237,6 +61,11 @@ export class InjectContext {
   get hooks()
   {
     return this._hooks;
+  }
+
+  get libraries()
+  {
+    return this._libraries;
   }
 
   get variables()
@@ -263,7 +92,31 @@ export class InjectContext {
   {
     const fileUrl = url.pathToFileURL(filename);
     const module = await import(fileUrl);
-    this._config = transformCMakeConfigToObject(module.default);
+    const originConfig = module.default;
+
+    const config = {};
+    for (const [key, entry] of Object.entries(originConfig)) {
+      if (entry.type === "boolean") {
+        config[key] = entry.value.toUpperCase() === "ON" || entry.value.toUpperCase() === "TRUE";
+      }
+      else if (entry.type === "number") {
+        config[key] = entry.value;
+      }
+      else if (entry.type === "string") {
+        config[key] = entry.value;
+      }
+      else if (entry.type === "strings") {
+        config[key] = entry.value ? entry.value.split(";") : [];
+      }
+      else if (Array.isArray(entry.type)) {
+        config[key] = entry.value;
+      }
+      else {
+        throw `Not supported type ${entry.type}`;
+      }
+    }
+
+    this._config = config;
   }
 
   async loadPlugin(filename)
--- scripts/run/InitVariables.mjs
+++ scripts/run/InitVariables.mjs
@@ -1,3 +1,7 @@
+import url from 'node:url';
+import * as CXX from "###/utils/CXX.js";
+import * as CMake from "###/utils/CMake.js";
+
 export default async function(ctx)
 {
   const {input, outputHeader, outputObject, outputScript, print} = ctx.args;
@@ -6,18 +10,130 @@ export default async function(ctx)
     throw "Not pass the input filename to the program";
   }
 
-  await ctx.variables.loadConfig(input);
-  await ctx.variables.triggerEvent();
+  const fileUrl = url.pathToFileURL(input);
+  const module = await import(fileUrl);
+  const config = module.default;
+
+  await ctx.hooks.emit("variables.init", config);
 
   if (outputScript) {
-    await ctx.variables.saveInitConfigAsCMake(outputScript, print);
+    const lines = [];
+
+    let maxLength = 0;
+    for (const name of Object.keys(config))
+      maxLength = Math.max(maxLength, name.length);
+    const leader = " .".repeat((maxLength + 4) / 2);
+    
+    lines.push(CMake.generatedScriptNameComment(ctx.entryScript));
+    for (const [name, entry] of Object.entries(config)) {
+      const space = entry.depends ? "  " : "";
+      if (entry.depends) {
+        lines.push(`if (${entry.depends})`);
+      }
+      const description = entry.description || "";
+      if (typeof entry.value === "boolean") {
+        lines.push(`${space}option(${name} "${description}" ${entry.value ? "ON" : "OFF"})`);
+      }
+      else if (typeof entry.value === "number") {
+        lines.push(`${space}set(${name} ${entry.value} CACHE STRING "${description}")`);
+      }
+      else if (typeof entry.value === "string") {
+        lines.push(`${space}set(${name} "${entry.value}" CACHE STRING "${description}")`);
+        if (Array.isArray(entry.type)) {
+          const strings = entry.type.map((i) => '"' + i + '"').join(' ');
+          lines.push(`${space}set_property(CACHE ${name} PROPERTY STRINGS ${strings})`);
+        }
+      }
+      else if (Array.isArray(entry.value)) {
+        lines.push(`${space}set(${name} "${entry.value}" CACHE STRING "${description}")`);
+      }
+      else {
+        throw `Not supported value of ${entry.value}`;
+      }
+      if (print) {
+        lines.push(`${space}message(STATUS "${name}:${leader.substring(name.length)} \${${name}}")`);
+      }
+      if (entry.depends) {
+        lines.push("endif ()");
+      }
+      lines.push("");
+    }
+  
+    await ctx.fs.linesSaveTo(outputScript, lines);
   }
 
   if (outputHeader) {
-    await ctx.variables.saveInitConfigAsCHeader(outputHeader);
+    const lines = [];
+  
+    lines.push(CXX.generatedScriptNameComment(ctx.entryScript));
+    lines.push("");
+  
+    for (const [name, entry] of Object.entries(config)) {
+      if (entry.description) {
+        lines.push(`/* ${entry.description} */`);
+      }
+      if (typeof entry.value === "boolean") {
+        lines.push(`#cmakedefine01 ${name}`);
+      }
+      else if (typeof entry.value === "number") {
+        lines.push(`#define ${name} @${name}@`);
+      }
+      else if (typeof entry.value === "string") {
+        lines.push(`#define ${name} "@${name}@"`);
+      }
+      else if (Array.isArray(entry.value)) {
+        lines.push(`#define ${name} "@${name}@"`);
+      }
+      else {
+        throw `Not supported value of ${entry.value}`;
+      }
+      lines.push("");
+    }
+  
+    await ctx.fs.linesSaveTo(outputHeader, lines);
   }
 
   if (outputObject) {
-    await ctx.variables.saveInitConfigAsModule(outputObject);
+    const lines = [];
+
+    lines.push(CXX.generatedScriptNameComment(ctx.entryScript));
+    lines.push("export default {");
+
+    for (const [name, entry] of Object.entries(config)) {
+      let type = entry.type;
+      let value = `@${name}@`;
+
+      if (type === undefined) {
+        if (typeof entry.value === "boolean") {
+          type = "boolean";
+        }
+        else if (typeof entry.value === "number") {
+          type = "number";
+        }
+        else if (typeof entry.value === "string") {
+          type = "string";
+        }
+        else if (Array.isArray(entry.value)) {
+          type = "strings";
+        }
+        else {
+          throw `Not supported value of ${entry.value}`;
+        }
+      }
+
+      if (type !== "number") {
+        value = '"' + value + '"';
+      }
+
+      lines.push(`  ${name}: {`);
+      lines.push(`    type: ${JSON.stringify(type)},`);
+      lines.push(`    value: ${value},`);
+      lines.push(`  },`);
+    }
+
+    lines.push("};");
+    lines.push("");
+
+    await ctx.fs.linesSaveTo(outputObject, lines);
   }
 }
