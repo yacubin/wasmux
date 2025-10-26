4e440693790cc90d90bbfa461e8c2cb33cfc82cc
Yurii Yakubin <yurii.yakubin@gmail.com>
2025-10-26 13:07:49 +0000

    Removed CXX from wchar Removed charactersReverseFind

--- uapi/string/src/memrchr.cpp
+++ uapi/string/src/memrchr.cpp
@@ -8,13 +8,17 @@
 #include <string.h>
 #include <stdlib.h>
 #include <wasmux/compiler.h>
-#include <wasmux/cxx/Characters.h>
 
 extern "C" void* __memrchr(const void* mem, int ch, size_t n)
 {
-  return const_cast<uint8_t*>(wasmux::charactersReverseFind<uint8_t,true>(
-    reinterpret_cast<const uint8_t*>(mem),
-    static_cast<uint8_t>(ch), n));
+  unsigned char* p = (unsigned char*)mem + n;
+  while (n != 0) {
+    if (*--p == ch)
+      return p;
+    n--;
+  }
+
+  return NULL;
 }
 
 extern "C" __ATTR_ALIAS(__memrchr, memrchr) __ATTR_WEAK;
