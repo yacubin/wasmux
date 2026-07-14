9e84530b62fe4135d7f690fe516ae9ced603f463
Yurii Yakubin <yurii.yakubin@gmail.com>
2026-07-14 21:40:22 +0100

    Implement stpcpy via strlen and bulk memcpy

--- uapi/string/src/stpcpy.c
+++ uapi/string/src/stpcpy.c
@@ -1,18 +1,19 @@
 /*
  *
- *  Copyright (C) 2025  Yurii Yakubin (yurii.yakubin@gmail.com)
+ *  Copyright (C) 2025-2026  Yurii Yakubin (yurii.yakubin@gmail.com)
  *
  */
 
 #include <string.h>
-#include <assert.h>
 #include <wasmux/export.h>
 #include <wasmux/compiler.h>
+#include <wasmux/string.h>
 
 char* __stpcpy(char* dst, const char* src)
 {
-  assert(0);
-  return NULL;
+  size_t len = wasmux_strlen(src);
+  __builtin_memcpy(dst, src, len + 1);
+  return dst + len;
 }
 
 __EXPORT __ATTR_WEAK_ALIAS(__stpcpy, stpcpy);
